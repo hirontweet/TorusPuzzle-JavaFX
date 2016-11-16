c3882effcda31e8d3e68ba8a35a71383e5ea4b55
Hirofumi NAKANO <jasdfhnakano@gmail.com>
2016-11-16 18:33:34 +0900

    [rotateBlock] ButtonでBlockを回す
    ハンドラの中にはfinalの変数しか入れられないため、独立したハンドラクラスを実装して対応。

---
+++ src/TorusCore/TorusEventHandlerHorizontal.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TorusCore;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+
+/**
+ *
+ * @author hiron_000
+ */
+public class TorusEventHandlerHorizontal implements EventHandler<ActionEvent>{
+
+    private int mIndex = 1;
+    private TorusPuzzle mTorus;
+    
+    public TorusEventHandlerHorizontal(TorusPuzzle torus, int index){
+        mTorus = torus;
+        mIndex = index;
+    }
+    
+    @Override
+    public void handle(ActionEvent event) {
+        mTorus.moveHorizontally(mIndex);
+    }
+    
+}
